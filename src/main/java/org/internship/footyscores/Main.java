43f79dd0aa7a217d5582865362fee31f0af7aba3
pmaciejonczyk <pmaciejonczyk@deloittece.com>
2026-09-25 10:58:18 +0200

    Program has now two subcommands: generate and serve

--- src/main/java/org/internship/footyscores/Main.java
+++ src/main/java/org/internship/footyscores/Main.java
@@ -1,17 +1,23 @@
 package org.internship.footyscores;
 
 import org.internship.footyscores.cli.GenerateCommand;
+import org.internship.footyscores.cli.ServeCommand;
 import picocli.CommandLine;
+import picocli.CommandLine.Command;
 
+@Command(
+    name = "footyscores",
+    mixinStandardHelpOptions = true,
+    version = "1.0",
+    description = "CLI tool for generating and serving Olympic football match data.",
+    subcommands = {GenerateCommand.class, ServeCommand.class})
 public final class Main {
 
-  private Main() {}
+  public Main() {}
 
   public static void main(String[] args) {
     int exitCode =
-        new CommandLine(new GenerateCommand())
-            .setCaseInsensitiveEnumValuesAllowed(true)
-            .execute(args);
+        new CommandLine(new Main()).setCaseInsensitiveEnumValuesAllowed(true).execute(args);
     System.exit(exitCode);
   }
 }
